8942c5d4c415e6b2a5a79e3fa000126f173dc856
geukfyger <geukfyger.anti0pass@gmail.com>
2014-11-17 15:55:39 +0900

    test3

--- Rep5/src/SemanticNet_ver_KT.java
+++ Rep5/src/SemanticNet_ver_KT.java
@@ -1,7 +1,48 @@
-	import java.util.*;
+	import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 //必須課題5−1で使うグループメンバーに関するセマンティックネット(仮)
 public class SemanticNet_ver_KT {
 	public static void main(String args[]){
 		SemanticNet sn = new SemanticNet();
+		String[] file = {"SemanticSample.txt"};
+		/**
+		String[] file = {"Semantic067.txt",
+				"Semantic088.txt",
+				"Semantic109.txt",
+				"Semantic110.txt",
+				"Semantic113.txt",};
+				*/
+		BufferedReader in;
+		Pattern p = Pattern.compile("(.*) (.*) (.*)");
+		Matcher m;
+		
+		for(int i = 0;i< file.length; i++){
+			try {    // ファイル読み込みに失敗した時の例外処理のためのtry-catch構文
+
+	            // 文字コードを指定してBufferedReaderオブジェクトを作る
+	            in = new BufferedReader(new InputStreamReader(new FileInputStream(file[i]), "UTF-8"));
+
+	         // 変数lineに1行ずつ読み込むfor文
+	            for (String line = in.readLine(); line != null; line = in.readLine()) {
+	            	m = p.matcher(line);
+	            	if(m.find()){
+	            		sn.addLink(new Link(m.group(2),m.group(1),m.group(3),sn));
+	            	}
+	            }
+
+	        } catch (IOException e) {
+	            e.printStackTrace(); // 例外が発生した所までのスタックトレースを表示
+	        }
+		}
+		
+		sn.printLinks();
+		sn.printNodes();
+		
+	}				
 }
