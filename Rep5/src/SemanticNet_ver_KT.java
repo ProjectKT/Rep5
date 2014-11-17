fd5e743602cdb2d5a2b1b66584ef38b766f5ba63
geukfyger <geukfyger.anti0pass@gmail.com>
2014-11-17 15:34:16 +0900

    test2

--- Rep5/src/SemanticNet_ver_KT.java
+++ Rep5/src/SemanticNet_ver_KT.java
@@ -1,4 +1,7 @@
+	import java.util.*;
+
 //必須課題5−1で使うグループメンバーに関するセマンティックネット(仮)
 public class SemanticNet_ver_KT {
-
+	public static void main(String args[]){
+		SemanticNet sn = new SemanticNet();
 }
