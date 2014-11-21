e3e48a5921eb15b8a32385885928ceac266ffbe6
geukfyger <geukfyger.anti0pass@gmail.com>
2014-11-21 15:40:56 +0900

    その２

---
+++ Rep5/src/Frame/AIDemonProc_OldBrother.java
@@ -0,0 +1,30 @@
+package Frame;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+
+public class AIDemonProc_OldBrother extends AIDemonProc {
+	
+	public Object eval(AIFrameSystem inFrameSystem, AIFrame inFrame,
+			String inSlotName, Iterator inSlotValues, Object inOpts) {
+		//
+		int data1 = (int)inFrame.readSlotValue(inFrameSystem, "誕生日", false);
+		System.out.println("a");
+		String papa = (String)inFrame.readSlotValue(inFrameSystem, "親", false);
+		AIFrame papaframe = inFrameSystem.get_Frame(papa);
+		ArrayList<String> list = papaframe.get_leankers_Slot_names("親");
+		for(int i=0;i<list.size();){
+			System.out.println(list.get(i));
+			AIFrame frame = inFrameSystem.get_Frame(list.get(i));
+			AIFrame x = (AIFrame)inFrameSystem.readSlotValue(list.get(i),"is-a");
+			if((int)frame.readSlotValue(inFrameSystem, "誕生日", false)>data1 && x.get_name().equals("男")){
+				i++;
+			}else{
+				list.remove(i);
+			}
+		}
+
+		return list;
+	}
+	
+}
