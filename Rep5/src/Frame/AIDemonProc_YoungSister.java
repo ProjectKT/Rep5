65c1da240b01462adbc3a205040c3ffde28d337d
Earver <cjh15109@stn.nitech.ac.jp>
2014-11-26 12:00:12 +0900

    Update AIDemonProc_YoungSister.java
    妹のモノを追加

--- Rep5/src/Frame/AIDemonProc_YoungSister.java
+++ Rep5/src/Frame/AIDemonProc_YoungSister.java
@@ -3,7 +3,7 @@ package Frame;
 import java.util.ArrayList;
 import java.util.Iterator;
 
-public class AIDemonProc_OldBrother extends AIDemonProc {
+public class AIDemonProc_ YoungSister extends AIDemonProc {
 
 	public Object eval(AIFrameSystem inFrameSystem, AIFrame inFrame,
 			String inSlotName, Iterator inSlotValues, Object inOpts) {
