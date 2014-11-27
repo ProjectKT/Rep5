41f0f0a5ba0079ce12b38b00d58ca2dd9eadb2f0
cjh15109 <cjh15109@cse.nitech.ac.jp>
2014-11-27 15:40:57 +0900

    息子、娘の追加

---
+++ Rep5/src/Frame/AIDemonProc_Daughter.java
@@ -0,0 +1,40 @@
+package Frame;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+
+public class AIDemonProc_Daughter {
+
+	public Object eval(AIFrameSystem inFrameSystem, AIFrame inFrame,
+			String inSlotName, Iterator inSlotValues, Object inOpts) {
+
+		// 条件を満たすものを入れるリスト
+		ArrayList<String> relist = new ArrayList<String>();
+
+		// 親の逆リンクから子供の名前(自分を親だと見て接続している名前)をとってくる
+		// klistは息子の名前のリスト
+			ArrayList<String> klist = inFrame
+					.get_leankers_Slot_names("親");
+
+		// 子供の数だけループ
+			for (int j = 0; j < klist.size(); j++) {
+
+				//子供の名前から子供のフレームをとってくる
+				AIFrame frame = inFrameSystem.get_Frame(klist.get(j));
+
+				// 性別スロットの値が男なら息子
+				if (frame.readSlotValue(inFrameSystem, "性別",
+								false).equals("女")) {
+
+					// 二回目以降のループ時すでに前のループで登録されてないか調べる
+					if (!relist.contains(klist.get(j))) {
+						// 初めての名前なら息子としてリストに入れる
+						relist.add(klist.get(j));
+					}
+				}
+			}
+		// relistを返す
+		return AIFrame.makeEnum(new ArrayList<String>(relist));
+	}
+	
+}
